9ef388935f02f6fc1f1a7d28f297632494ebc7ea
Maxim <gatilin@teamidea.ru>
2017-05-24 11:00:09 +0300

    Create more nice message about error #16

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
 import styles from './Error.css';
+import ErrorIcon from './../icons/ErrorIcon';
 
 class Error extends Component {
 	render() {
 			return (
 	    	<div className={styles.container}>
+	    		<ErrorIcon />
 	    		<h2 className={styles.text}>{this.props.children}</h2>
 	    	</div>
     	)
