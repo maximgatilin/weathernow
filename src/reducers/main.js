e9059fcedccc857bcaeaaa13fd2949862e0864a4
Maxim <gatilin@teamidea.ru>
2017-05-26 09:27:46 +0300

    Use spread operator instead Object.assign

--- src/reducers/main.js
+++ src/reducers/main.js
@@ -1,45 +1,48 @@
 const mainReducer = (state , action) => {
 	switch (action.type) {
 		case 'REQUEST_LOCATION':
-			return Object.assign({}, state, {
-				loading: true
-			})
+			return {...state, loading: true}
 		case 'RECEIVE_LOCATION':
-			return Object.assign({}, state, {
-			location: {
-				city: action.city,
-				latitude: action.latitude,
-				longitude: action.longitude
-			},
-			locationIsChanged: false
-		})
+			return {
+					...state,
+				location: {
+					city: action.city,
+					latitude: action.latitude,
+					longitude: action.longitude
+				},
+				locationIsChanged: false
+			}
 		case 'CHANGE_LOCATION':
-			return Object.assign({}, state, {
+			return {
+				...state,
 				location: {
 					city: action.city,
 					latitude: action.latitude,
 					longitude: action.longitude
 				},
 				locationIsChanged: true
-			})
+			}
 		case 'RECEIVE_WEATHER':
-			return Object.assign({}, state, {
+			return {
+				...state,
 				temperature: action.temperature,
 				weatherDescription: action.description,
 				weatherIcon: action.icon,
 				pageBackground: action.background,
 				loading: false,
 				loadingError: false
-			})
+			}
 		case 'SWITCH_EDIT_MODE':
-			return Object.assign({}, state, {
+			return {
+				...state,
 				editMode: action.value
-			})
+			}
 		case 'LOADING_FAILED':
-			return Object.assign({}, state, {
+			return {
+				...state,
 				loading: false,
 				loadingError: true
-			})
+			}
 		default:
 			return state;
 	}
